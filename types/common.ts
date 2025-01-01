e8a9ca124ef6a6dd446048e3e3efcb3249a7c100
Sibin Xavier <sibin.xavier@simelabs.com>
2025-01-01 14:56:24 +0530

    Move topics to data json,

---
+++ types/common.ts
@@ -0,0 +1,8 @@
+export type TechnologyStack = {
+	id: number;       // Represents the unique identifier
+	name: string;     // Name of the technology
+	type: string;     // Type of the technology (e.g., "database")
+	icon: string;     // CSS class or identifier for the icon
+	link: string;     // URL or link (can be empty)
+	color: string;    // Hex color code for the technology
+};
