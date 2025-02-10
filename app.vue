ab4602f34ed3db298ec66b494f8aefa5a9df1f18
Sibin Xavier <sibinx7@gmail.com>
2025-02-10 22:51:54 +0530

    Sidebar responsiveness

--- app.vue
+++ app.vue
@@ -2,7 +2,17 @@
   <div class="fix-bg">
 	  <div class="container">
 	    <div class="main">
-			  <div class="sidebar-wrapper">
+		    <div class="sidebar-toggle open-action" :class="!isSidebarOpen && 'd-none'">
+			    <button @click="handleClick()" class="btn btn-link">
+				    <i class="bi bi-list"></i>
+			    </button>
+		    </div>
+			  <div class="sidebar-wrapper" :class="isSidebarOpen && 'close'">
+				  <div class="sidebar-toggle close-action">
+					  <button @click="handleClick()" class="btn btn-link">
+						  <i class="bi bi-x-lg"></i>
+					  </button>
+				  </div>
 				  <div class="sidebar-wrapper-scrollable">
 					  <Sidebar/>
 				  </div>
@@ -18,4 +28,10 @@
 </template>
 <script setup>
 import Sidebar from "~/common/sidebar/sidebar.vue";
+import { ref } from "vue";
+const isSidebarOpen = ref(false);
+
+const handleClick = () => {
+	isSidebarOpen.value = !isSidebarOpen.value;
+}
 </script>
