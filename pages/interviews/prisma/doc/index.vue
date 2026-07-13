f5491b84c26d62253fe7ff3466c95d7930d572fe
Sibin Xavier <sibinx7@gmail.com>
2026-07-13 21:41:44 +0530

    Add prisma questions

---
+++ pages/interviews/prisma/doc/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import PageRender from '~/common/content/PageRender.vue';
+
+
+</script>
+
+<template>
+	<div class="common-page-render">
+		<PageRender/>
+	</div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
